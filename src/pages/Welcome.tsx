6c78c849cdef7833708cc455dd710b55633949a1
Lucas Almeida da Silva <lucas_almeidasilva01@hotmail.com>
2021-04-20 18:54:05 -0300

    Welcome, identification and confirmation screens

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,60 +1,86 @@
 import React from 'react';
 import {
-  SafeAreaView, 
-  Text, 
-  Image, 
-  StyleSheet 
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
-import { Button } from '../components/Button';
-
-import colors from '../../styles/colors';
 import wateringImage from '../assets/watering.png';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate('UserIdentification');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'} 
-        suas plantas {'\n'}
-        de forma fácil
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
       </Text>
 
-      <Image source={wateringImage} />
-      
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. 
-        Nós cuidados de lembrar você sempre que precisar
+        <Image
+          source={wateringImage}
+          resizeMode='contain'
+        />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidados de lembrar você sempre que precisar
       </Text>
 
-      <Button title={'>'} />
-      
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  wrapper: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 40
+    marginTop: 40,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7,
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text,
   },
   button: {
     backgroundColor: colors.green,
@@ -63,10 +89,10 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    width: 56
+    width: 56,
   },
-  buttonText: {
+  buttonIcon: {
+    fontSize: 32,
     color: colors.white,
-    fontSize: 24
   }
 });
